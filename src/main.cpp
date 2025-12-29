10e3c63cf67bb742ac9278a939a03b67946d0bfd
Dmitrii <dm.ed.orlov@gmail.com>
2025-12-29 20:58:48 +0200

    upd: Cleanup request to not wait for time sync

--- src/main.cpp
+++ src/main.cpp
@@ -63,32 +63,6 @@ void requestData()
     return;
   }
 
-  // Wait for sync
-  struct tm timeinfo;
-  int retries = 0;
-  // getLocalTime is a built-in helper that handles the wait for you
-  while (!getLocalTime(&timeinfo) && retries < 15)
-  {
-    Serial.print(".");
-    delay(1000);
-    retries++;
-  }
-
-  if (retries < 15)
-  {
-    Serial.println("\nTime synchronized successfully!");
-    Serial.printf("Current time: %02d:%02d:%02d\n", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
-    Serial.printf(
-        "%04d-%02d-%02d %02d:%02d:%02d\n",
-        timeinfo.tm_year + 1900,
-        timeinfo.tm_mon + 1,
-        timeinfo.tm_mday,
-        timeinfo.tm_hour,
-        timeinfo.tm_min,
-        timeinfo.tm_sec);
-    Serial.printf("Epoch time: %ld\n", time(nullptr));
-  }
-
   isRequestInProgress = true;
   GetTransportRequest request;
   GetTransportResponse *response =
@@ -104,9 +78,6 @@ void requestData()
     int startY = statusBar->getHeight() + btnRefresh.h + 40;
     int labelHeight = 40;
     int labelWidth = 300;
-
-    M5.Display.startWrite();
-    M5.Display.fillRect(startX, startY, M5.Display.width() - startX, M5.Display.height() - startY, TFT_WHITE);
 
     time_t utcTime = time(nullptr);
     for (int i = 0; i < countToShow; i++)
@@ -131,11 +102,9 @@ void requestData()
       timeLabel.value = secondsDiffString;
       timeLabels[i] = timeLabel;
     }
-    M5.Display.endWrite();
 
     delete response;
 
-    char statusText[255];
     String timeStr = timestampToDatetime(utcTime);
     statusBar->setValue((String("Data refreshed at ") + timeStr).c_str());
   }
